1d7dee8b16467d0b6cde610d63e2a8e62344ad6c
AndresTube <andresfelipegrado4@gmail.com>
2026-01-29 23:17:18 -0500

    center help message

--- src/main/java/com/fendrixx/iLikeHomes/commands/ReloadCommand.java
+++ src/main/java/com/fendrixx/iLikeHomes/commands/ReloadCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import com.fendrixx.iLikeHomes.Main;
+import com.fendrixx.iLikeHomes.utils.TextCenter;
 
 public class ReloadCommand implements CommandExecutor {
     private Main plugin;
@@ -16,55 +17,46 @@ public class ReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
-            // idk how to center a message soooo
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a======== §6i§eLike§6Homes §aHelp ========");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/sethome <name> §7- Set a home");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/home <name> §7- Teleport to a home");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/delhome <name> §7- Delete a home");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/homes §7- Open a GUI with all your homes");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/ilikehomes reload §7- Reload the plugin");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/ilikehomes help §7- Shows this message");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a=========================================");
-            return true;
-            // this is a foking disaster LOL
-        }
-        // check permissions
-        if (!sender.hasPermission("ilikehomes.admin")) {
-            sender.sendMessage("§8[§6i§eLike§6Homes§8] §cYou don't have permission to use this command!");
+            String text = "§a======== §6i§eLike§6Homes §aHelp ========\n"
+                    + "§a/sethome <name> §7- Set a home\n"
+                    + "§a/home <name> §7- Teleport to a home\n"
+                    + "§a/delhome <name> §7- Delete a home\n"
+                    + "§a/homes §7- Open a GUI with all your homes\n"
+                    + "§a/ilikehomes reload §7- Reload the plugin\n"
+                    + "§a/ilikehomes help §7- Shows this message\n"
+                    + "§a=========================================";
+            String centered = TextCenter.centerText(text);
+            sender.sendMessage(centered);
             return true;
         }
 
         if (args[0].equalsIgnoreCase("reload")) {
-            plugin.reloadPluginConfig();
-            sender.sendMessage("§8[§6i§eLike§6Homes§8] §aPlugin reloaded!");
+            if (sender.hasPermission("ilikehomes.reload")) {
+                plugin.reloadPluginConfig();
+                sender.sendMessage("§8[§6i§eLike§6Homes§8] §aPlugin reloaded!");
+                return true;
+            }
+            sender.sendMessage("§8[§6i§eLike§6Homes§8] §cYou don't have permission to use this command!");
             return true;
         }
 
         if (args[0].equalsIgnoreCase("help")) {
-            // idk how to center a message x2
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a======== §6i§eLike§6Homes §aHelp ========");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/sethome <name> §7- Set a home");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/home <name> §7- Teleport to a home");
-            sender.sendMessage("§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/delhome <name> §7- Delete a home");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/homes §7- Open a GUI with all your homes");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/ilikehomes reload §7- Reload the plugin");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a/ilikehomes help §7- Shows this message");
-            sender.sendMessage(
-                    "§r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §r §a=========================================");
+            if (sender.hasPermission("ilikehomes.help")) {
+                String text = "§a======== §6i§eLike§6Homes §aHelp ========\n"
+                        + "§a/sethome <name> §7- Set a home\n"
+                        + "§a/home <name> §7- Teleport to a home\n"
+                        + "§a/delhome <name> §7- Delete a home\n"
+                        + "§a/homes §7- Open a GUI with all your homes\n"
+                        + "§a/ilikehomes reload §7- Reload the plugin\n"
+                        + "§a/ilikehomes help §7- Shows this message\n"
+                        + "§a=========================================";
+                String centered = TextCenter.centerText(text);
+                sender.sendMessage(centered);
+                return true;
+            }
+            sender.sendMessage("§8[§6i§eLike§6Homes§8] §cYou don't have permission to use this command!");
             return true;
-            // this is a foking disaster bro, pls make a method to just put idk,
-            // sender.sendCenteredMessage()
         }
-
         return true;
     }
 }
